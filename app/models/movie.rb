2840a792fb44205d57dee41835152dcda161a30a
arnaud cormier <arno.cormier@gmail.com>
2019-06-18 00:45:50 +0200

    add minimal validation for movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true, length: { minimum: 4 }
+
     def flop?
       total_gross.blank? || total_gross < 225_000_000
     end
