803f4d84d988f73aa8c580809c0325e24cd3d8a4
arnaud cormier <arno.cormier@gmail.com>
2019-06-18 00:42:07 +0200

    delete movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-    before_action :set_movie, only: [:show, :edit, :update]
+    before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
     def index
         @movies = Movie.all
@@ -26,6 +26,11 @@ class MoviesController < ApplicationController
     def update
         @movie.update(movie_params)
         redirect_to @movie
+    end
+
+    def destroy
+        @movie.destroy
+        redirect_to movies_url
     end
 
     private
