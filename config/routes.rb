0d57dafae2973781aeaabe853b2641805b7f4592
arnaud cormier <arno.cormier@gmail.com>
2019-06-17 23:32:40 +0200

    udpate movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "movies#index"
   get "movies", to: "movies#index"
   get "movies/:id", to: "movies#show", as: "movie"
+  get "movies/:id/edit", to: "movies#edit", as: "edit_movie"
 end
