68ea937feb6c24219dfc5978b86657f0213dff7d
arnaud cormier <arno.cormier@gmail.com>
2019-06-17 23:52:58 +0200

    refactoring controller with private method and callback

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,30 @@
 class MoviesController < ApplicationController
+    before_action :set_movie, only: [:show, :edit, :update]
+
     def index
         @movies = Movie.all
     end
 
     def show
-       @movie = Movie.find(params[:id])
+       
     end
 
     def edit
-        @movie = Movie.find(params[:id])
+        
     end
 
     def update
-        @movie = Movie.find(params[:id])
-
-        movie_params =
-            params.require(:movie).
-            permit(:title, :description, :rating, :released_on, :total_gross)
-
         @movie.update(movie_params)
-        
         redirect_to @movie
+    end
+
+    private
+
+    def set_movie
+        @movie = Movie.find(params[:id])
+    end
+
+    def movie_params
+        params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
     end
 end
